a9ad664d92a7795d432ea04894c7e3a992aca3eb
KelWin27 <kelwin90210@gmail.com>
2023-04-19 22:29:15 +0300

    lesson2(dop)

--- js/components/createPairs.js
+++ js/components/createPairs.js
@@ -2,48 +2,44 @@ import { createElement } from "../helper/createElement.js"
 
 export const createPairs = (app) => {
 
-    const createCard = (data) => {
+    const createPair = createElement('section', {
+        className: 'card section-offset',
+    });
 
-        const createPair = createElement('section', {
-            className: 'card section-offset',
-        });
-        
-        const cardContainer = createElement('div', {
-            className: 'container card__container',
-        });
-        
-        const buttonReturnCard = createElement('button', {
-            className: 'card__return',
-            ariaLabel: 'Возврат к категориям',
-        });
-        
-        const buttonCardItem = createElement('button', {
-            className: 'card__item',
-        });
-        
-        const spanCardItemFront = createElement('span', {
-            className: 'card__front',
-            textContent: data[0],
-        });
-        
-        const spanCardItemBack = createElement('span', {
-            className: 'card__back',
-            textContent: data[1],
-        });
-        buttonCardItem.append(spanCardItemFront, spanCardItemBack);
-        
-        cardContainer.append(buttonReturnCard, buttonCardItem);
-        createPair.append(cardContainer);
+    const cardContainer = createElement('div', {
+        className: 'container card__container',
+    });
+    
+    const buttonReturnCard = createElement('button', {
+        className: 'card__return',
+        ariaLabel: 'Возврат к категориям',
+    });
 
-        return createPair;
-    }
-        
+    const buttonCardItem = createElement('button', {
+        className: 'card__item',
+    });
+    
+    const spanCardItemFront = createElement('span', {
+        className: 'card__front',
+        textContent: '',
+    });
+    
+    const spanCardItemBack = createElement('span', {
+        className: 'card__back',
+        textContent: '',
+    });
+    createPair.append(cardContainer)
+    cardContainer.append(buttonReturnCard, buttonCardItem);
+    buttonCardItem.append(spanCardItemFront, spanCardItemBack);
+
     const mount = (data) => {
-        app.append(createCard(data));
+        spanCardItemFront.textContent = data[0];
+        spanCardItemBack.textContent = data[1];
+        app.append(createPair);
     };
 
     const unMount = () => {
-        createCard.remove();
+        createPair.remove();
     };
 
     return {mount, unMount};
--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 import { createCategory } from "./components/createCategory.js";
 import { createEditCategory } from "./components/createEditCategory.js";
 import { createHeader } from "./components/createHeader.js";
-// import { createPairs } from "./components/createPairs.js";
+import { createPairs } from "./components/createPairs.js";
 import { createElement } from "./helper/createElement.js";
 import { fetchCards, fetchCategories } from "./service/apiService.js";
 
@@ -12,10 +12,10 @@ const initApp = async () => {
     const headerObject = createHeader(headerParent);
     const categoryObject = createCategory(app);
     const editCategoryObject = createEditCategory(app);
-    // const pairsObject = createPairs(app);
+    const pairsObject = createPairs(app);
 
     const allSectionUnmount = () => {
-        [categoryObject, editCategoryObject].forEach(obj => obj.unMount());
+        [categoryObject, editCategoryObject, pairsObject].forEach(obj => obj.unMount());
     };
 
     const renderIndex = async e => {
@@ -53,13 +53,14 @@ const initApp = async () => {
         }
     });
 
-    // categoryObject.categoryList.addEventListener('click', async ({ target }) => {
-    //     const categoryItem = target.closest('.category__item');
-    //     const dataCard = await fetchCards(categoryItem.dataset.id);
-    //     pairsObject.mount(dataCard.pairs[0])
-    //     return;
-    //     }
-    // );
+    categoryObject.categoryList.addEventListener('click', async ({ target }) => {
+        allSectionUnmount();
+        const categoryItem = target.closest('.category__item');
+        const dataCard = await fetchCards(categoryItem.dataset.id);
+        pairsObject.mount(dataCard.pairs[0])
+        return;
+        }
+    );
 }
 
 initApp()
