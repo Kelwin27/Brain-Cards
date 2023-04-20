5a262d5785bc258aa03f12bea43ebc64272413bf
KelWin27 <kelwin90210@gmail.com>
2023-04-20 13:03:24 +0300

    day 3

--- js/components/createPairs.js
+++ js/components/createPairs.js
@@ -1,4 +1,6 @@
-import { createElement } from "../helper/createElement.js"
+import { createElement } from "../helper/createElement.js";
+import { shuffleArr } from "../helper/shuffleArr.js";
+import { showAlert } from "./showAlert.js";
 
 export const createPairs = (app) => {
 
@@ -32,9 +34,40 @@ export const createPairs = (app) => {
     cardContainer.append(buttonReturnCard, buttonCardItem);
     buttonCardItem.append(spanCardItemFront, spanCardItemBack);
 
+    const cardController = data => {
+        let index = 0;
+        spanCardItemFront.textContent = data[index][0];
+        spanCardItemBack.textContent = data[index][1];
+        const flipCard = () => {
+            buttonCardItem.classList.add('card__item_flipped');
+            buttonCardItem.removeEventListener('click', flipCard);
+            console.log(index)
+            setTimeout(() => {
+                buttonCardItem.classList.remove('card__item_flipped');
+                setTimeout(() => {
+                    index++;
+                    if (index === data.length) {
+                        spanCardItemFront.textContent = 'the end';
+                        showAlert('Вернемся к категориям', 2000);
+                        setTimeout(() => {
+                            buttonReturnCard.click();
+                        }, 2000);
+                        return;
+                    };
+                    spanCardItemFront.textContent = data[index][0];
+                    spanCardItemBack.textContent = data[index][1];
+                    setTimeout(() => {
+                        buttonCardItem.addEventListener('click', flipCard);
+                    }, 200);
+                }, 100);
+            }, 1000);
+        };
+        buttonCardItem.addEventListener('click', flipCard);
+    };
+
     const mount = (data) => {
-        spanCardItemFront.textContent = data[0];
-        spanCardItemBack.textContent = data[1];
+        const tempArr = shuffleArr(data.pairs)
+        cardController(tempArr);
         app.append(createPair);
     };
 
@@ -42,5 +75,5 @@ export const createPairs = (app) => {
         createPair.remove();
     };
 
-    return {mount, unMount};
+    return { buttonReturnCard, mount, unMount };
 }
